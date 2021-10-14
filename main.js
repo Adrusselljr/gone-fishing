8d9a939833205bee6594ac2fdeb2dc405c9b27b9
Aaron Russell Jr <adrusselljr@Aarons-MBP.lan>
2021-10-14 12:09:10 -0400

    modified js

--- main.js
+++ main.js
@@ -7,17 +7,17 @@ console.log(chalk.whiteBright("=========================================="))
 console.log("")
 
 let fishKept = []
-let hour = 6
+let hour = 6.00
 
-while(hour <= 12) {
+while(true) {
 
     let name = ""
     let weight = 0
     let value = 0
 
-    if(hour === 12) {
+    if(hour >= 12.00) {
 
-        console.log(`The time is ${hour}:00pm.  Times up!`)
+        console.log(`The time is ${Number(hour.toFixed(2))}pm.  Times up!`)
         console.log("")
         console.log(`You caught ${fishKept.length} fish:`)
 
@@ -29,14 +29,14 @@ while(hour <= 12) {
 
         }
         console.log("")
-        console.log("Total weight:", chalk.yellow(`${weight.toFixed(Number(2))} lbs`))
-        console.log("Total weight:", chalk.yellow(`$${value.toFixed(Number(2))}`))
+        console.log("Total weight:", chalk.yellow(`${Number(weight.toFixed(2))} lbs`))
+        console.log("Total weight:", chalk.yellow(`$${Number(value.toFixed(2))}`))
         break
 
     }
     else {
 
-        console.log(`The Time is ${hour}:00am.  So far you've caught:`)
+        console.log(`The Time is ${Number(hour.toFixed(2))}am.  So far you've caught:`)
         console.log(display())
 
     }
@@ -48,7 +48,7 @@ while(hour <= 12) {
             weight += fishKept[i].weight
             value += fishKept[i].value
         }
-        return `${chalk.yellow(fishKept.length)} fish, ${chalk.yellow(weight.toFixed(Number(2)))} lbs, $${chalk.yellow(value.toFixed(Number(2)))}`
+        return `${chalk.yellow(fishKept.length)} fish, ${chalk.yellow(Number(weight.toFixed(2)))} lbs, ${chalk.yellow('$' + Number(value.toFixed(2)))}`
 
     }
     console.log("")
@@ -58,20 +58,24 @@ while(hour <= 12) {
         const fish = {
             name: "",
             weight: 0,
-            value: 0
+            value: 0,
+            time: 0
         }
         
         const fishAdj1 = ["Slimy", "Grey", "Purple", "Deepsea", "Tropical", "Slippery", "Tasteless", "Large", "Vicious", "Sharp-jawed"]
         const fishAdj2 = ["Scaly", "Finned", "Bottom-dwelling", "Bigmouthed", "Smallmouthed", "Pickled", "Poisonous", "Red-bellied", "Freshwater", "Ferocious"]
         const fishType = ["Blobfish", "Bass", "Salmon", "Herring", "Angler", "Carp", "Guppy", "Goldfish", "Northern Pike", "Catfish"]
+        const time = [0.15, 0.25, 0.50, 0.75]
     
         fish.name = `${fishAdj1[Math.ceil(Math.random() * fishAdj1.length - 1)]} ${fishAdj2[Math.ceil(Math.random() * fishAdj2.length - 1)]} ${fishType[Math.ceil(Math.random() * fishType.length - 1)]}`
     
         fish.weight = Math.ceil(Math.random() * 1000) / 100
     
         fish.value = Math.ceil(Math.random() * 1000) / 100
+
+        fish.time = time[Math.ceil(Math.random() * time.length - 1)]
         
-        console.log(`You caught a '${chalk.yellow(fish.name)}' weighing ${chalk.yellow(fish.weight)} lbs with a value of $${chalk.yellow(fish.value)} !`)
+        console.log(`You caught a '${chalk.yellow(fish.name)}' weighing ${chalk.yellow(fish.weight)} lbs with a value of ${chalk.yellow('$' + fish.value)}, and took ${chalk.yellow(fish.time)} hours!`)
     
         return fish
     
@@ -85,6 +89,7 @@ while(hour <= 12) {
         console.log("")
         console.log("Press [enter] to continue.")
         prompt()
+        console.log(chalk.whiteBright("=========================================="))
 
     }
     else {
@@ -111,6 +116,6 @@ while(hour <= 12) {
 
     }
 
-    hour++
+    hour += fish.time
 
 }
